ccd01d657417d778c0e5566a2f0b06589b1383fc
Auryx <lilbizb1@gmail.com>
2023-10-07 00:38:58 -0700

    Working

--- todos/serializers.py
+++ todos/serializers.py
@@ -8,4 +8,4 @@ class TodoSerializer(serializers.HyperlinkedModelSerializer):
         # The model it will serialize
         model = Cheese
         # the fields that should be included in the serialized output
-        fields = "__all__"
+        fields = ('id', 'name', 'origin_country', 'type')
